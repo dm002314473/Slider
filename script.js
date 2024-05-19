2a64051b22c923d7d27e1ac1883585d38ecf37d0
Duje Milic <dm002314473@fer.hr>
2024-05-19 14:50:08 +0200

    ispravljeni komentari

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(() => {
   // Function to animate the scrolling of images to the right
-  function animateRowNext(row) {
-    var $lastImage = row.children("img").last();
-    var $imageMargin = parseInt($lastImage.css("margin-right"));
-    var $lastImageWidth = $lastImage.outerWidth(true);
-    var $lastImageClone = $lastImage.clone();
+  const animateRowNext = (row) => {
+    const $lastImage = row.children("img").last();
+    const $imageMargin = parseInt($lastImage.css("margin-right"));
+    const $lastImageWidth = $lastImage.outerWidth(true);
+    const $lastImageClone = $lastImage.clone();
 
     // Prepending a clone of the last image to the beginning of the row
     $lastImageClone
@@ -12,23 +12,20 @@ $(document).ready(function () {
       .css({ marginLeft: $imageMargin, opacity: 1 });
 
     // Animating the disappearance of the original last image
-    $lastImage.animate(
-      { marginLeft: `-=${$lastImageWidth}px`, opacity: 0 },
-      1000
-    );
+    $lastImage.animate({ marginRight: `-=${$lastImageWidth}px` }, 1000);
 
     // Removing the original last image from the row after animation
-    setTimeout(function () {
+    setTimeout(() => {
       $lastImage.remove();
     }, 1000);
-  }
+  };
 
   // Function to animate the previous row of images
-  function animateRowPrev(row) {
-    var $firstImage = row.children("img").first();
-    var $imageMargin = parseInt($firstImage.css("margin-left"));
-    var $firstImageWidth = $firstImage.outerWidth(true);
-    var $firstImageClone = $firstImage.clone();
+  const animateRowPrev = (row) => {
+    const $firstImage = row.children("img").first();
+    const $imageMargin = parseInt($firstImage.css("margin-left"));
+    const $firstImageWidth = $firstImage.outerWidth(true);
+    const $firstImageClone = $firstImage.clone();
 
     // Appending a clone of the first image to the end of the row
     $firstImageClone
@@ -36,34 +33,36 @@ $(document).ready(function () {
       .css({ marginLeft: -$firstImageWidth + $imageMargin, opacity: 0 });
 
     // Animating the appearance of the first image at the end of the row
-    $firstImageClone.animate({ marginLeft: $imageMargin, opacity: 1 }, 1000);
+    $firstImageClone.animate({ marginLeft: $imageMargin, opacity: 0 }, 500);
+    $firstImageClone.animate({ marginLeft: $imageMargin, opacity: 1 }, 500);
 
     // Removing the original first image from the row after animation
-    setTimeout(function () {
+    setTimeout(() => {
       $firstImage.remove();
     }, 1000);
-  }
+  };
 
   // Handling click events for next and previous buttons
-  $(".next, .prev").click(function () {
+  $(".next, .prev").on("click", function () {
     // Disabling buttons while animation is ongoing to prevent multiple clicks
     $(".next, .prev").prop("disabled", true);
 
-    var $button = $(this);
-    var $upperRow = $(".upper-row");
-    var $lowerRow = $(".lower-row");
+    // Now "event.target" refers to the clicked button
+    const $button = $(this);
+    const $upperRow = $(".upper-row");
+    const $lowerRow = $(".lower-row");
 
     // Determining which direction to animate based on the clicked button
     if ($button.hasClass("next")) {
       animateRowNext($upperRow);
       animateRowNext($lowerRow);
-    } else {
+    } else if ($button.hasClass("prev")) {
       animateRowPrev($upperRow);
       animateRowPrev($lowerRow);
     }
 
     // Enabling buttons after animation is complete
-    setTimeout(function () {
+    setTimeout(() => {
       $(".next, .prev").prop("disabled", false);
     }, 1000);
   });
---
+++ script.transpiled.js
@@ -0,0 +1,81 @@
+"use strict";
+
+$(document).ready(function () {
+  // Function to animate the scrolling of images to the right
+  var animateRowNext = function animateRowNext(row) {
+    var $lastImage = row.children("img").last();
+    var $imageMargin = parseInt($lastImage.css("margin-right"));
+    var $lastImageWidth = $lastImage.outerWidth(true);
+    var $lastImageClone = $lastImage.clone();
+
+    // Prepending a clone of the last image to the beginning of the row
+    $lastImageClone.prependTo(row).css({
+      marginLeft: $imageMargin,
+      opacity: 1
+    });
+
+    // Animating the disappearance of the original last image
+    $lastImage.animate({
+      marginRight: "-=".concat($lastImageWidth, "px")
+    }, 1000);
+
+    // Removing the original last image from the row after animation
+    setTimeout(function () {
+      $lastImage.remove();
+    }, 1000);
+  };
+
+  // Function to animate the previous row of images
+  var animateRowPrev = function animateRowPrev(row) {
+    var $firstImage = row.children("img").first();
+    var $imageMargin = parseInt($firstImage.css("margin-left"));
+    var $firstImageWidth = $firstImage.outerWidth(true);
+    var $firstImageClone = $firstImage.clone();
+
+    // Appending a clone of the first image to the end of the row
+    $firstImageClone.appendTo(row).css({
+      marginLeft: -$firstImageWidth + $imageMargin,
+      opacity: 0
+    });
+
+    // Animating the appearance of the first image at the end of the row
+    $firstImageClone.animate({
+      marginLeft: $imageMargin,
+      opacity: 0
+    }, 500);
+    $firstImageClone.animate({
+      marginLeft: $imageMargin,
+      opacity: 1
+    }, 500);
+
+    // Removing the original first image from the row after animation
+    setTimeout(function () {
+      $firstImage.remove();
+    }, 1000);
+  };
+
+  // Handling click events for next and previous buttons
+  $(".next, .prev").on("click", function () {
+    // Disabling buttons while animation is ongoing to prevent multiple clicks
+    $(".next, .prev").prop("disabled", true);
+
+    // Now "event.target" refers to the clicked button
+    var $button = $(this);
+    var $upperRow = $(".upper-row");
+    var $lowerRow = $(".lower-row");
+
+    // Determining which direction to animate based on the clicked button
+    if ($button.hasClass("next")) {
+      animateRowNext($upperRow);
+      animateRowNext($lowerRow);
+    } else if ($button.hasClass("prev")) {
+      animateRowPrev($upperRow);
+      animateRowPrev($lowerRow);
+    }
+
+    // Enabling buttons after animation is complete
+    setTimeout(function () {
+      $(".next, .prev").prop("disabled", false);
+    }, 1000);
+  });
+});
